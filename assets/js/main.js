7c103de4c5621bfe9ad10afda6e19c782b748693
Luis-Mocha <luismocha97@gmail.com>
2023-03-27 16:56:42 +0200

    fine esercizio (da migliorare graficamente)

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,26 +18,61 @@ playButton.addEventListener('click', function () {
 
     if (myDice > rivalDice) {
         console.log('Hai vinto');
-        outcomeDiv.innerHTML = '<h2 class="text-center">Hai Vinto!</h2>'
+        outcomeDiv.innerHTML = '<h2 class="text-center">Hai Vinto!</h2>';
     }
     else if (myDice < rivalDice) {
-        console.log('Hai perso')
-        outcomeDiv.innerHTML = '<h2 class="text-center">Hai Perso!</h2>'
+        console.log('Hai perso');
+        outcomeDiv.innerHTML = '<h2 class="text-center">Hai Perso!</h2>';
     }
     else {
-        console.log('Pareggio!')
-        outcomeDiv.innerHTML = '<h2 class="text-center">Pareggio!</h2>'
+        console.log('Pareggio!');
+        outcomeDiv.innerHTML = '<h2 class="text-center">Pareggio!</h2>';
     };
 
     // My hand img
     let myHand = document.querySelector('#myHand');
     myHand.innerHTML = 
-    `<img src="./assets/img/dice-${myDice}.svg" alt="Immagine dado giocatore" class="dice-img">`
-    console.log(myHand)
+    `<img src="./assets/img/dice-${myDice}.svg" alt="Immagine dado giocatore" class="dice-img">`;
     // Rival Hand img
     let rivalHand = document.querySelector('#rivalHand');
     rivalHand.innerHTML = 
-    `<img src="./assets/img/dice-${rivalDice}.svg" alt="Immagine dado giocatore" class="dice-img">`
-    console.log(rivalHand)
+    `<img src="./assets/img/dice-${rivalDice}.svg" alt="Immagine dado giocatore" class="dice-img">`;
 
 });
+
+// VALIDATION FORM
+
+const validationDiv = document.querySelector('#validationDiv');
+const loginButton = document.querySelector('#loginButton');
+const diceGame = document.querySelector('#diceGame');
+let welcomeText = document.querySelector('main h1');
+
+let emailList = ['player1@gmail.com', 'player2@live.it', 'player3@gmail.com', 'player4@gmail.com', 'player5@live.it'];
+
+
+loginButton.addEventListener('click', function (invioForm) {
+    //Evito che si ricarichi il sito
+    invioForm.preventDefault();
+
+    // valori del form
+    let emailValue = document.querySelector('#floatingEmail').value;
+    const floatingName = document.querySelector('#floatingName').value;
+    const floatingSurname = document.querySelector('#floatingSurname').value;
+
+
+    if (emailList.includes(emailValue)) {
+        validationDiv.classList.add("d-none");
+        diceGame.classList.remove("d-none")
+        welcomeText.innerHTML = `Benvenuto ${floatingName} ${floatingSurname}`;
+    }
+    else {
+        alert("E-MAIL NON VALIDA. NON HAI ACCESSO A QUESTO SERVIZIO - TORNA INDIETRO");
+    }
+
+});
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', function() {
+    window.location.reload();
+});
+
